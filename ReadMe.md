528c9e893d88183dd9e89355428a48e88da1fc06
Raum Dellamorte <elementalsin@gmail.com>
2024-08-03 22:35:44 -0800

    Now With Fancy TUI via Cursive

--- ReadMe.md
+++ ReadMe.md
@@ -2,17 +2,20 @@
 ### Running Wayland and can't use GreenWithEnvy? Relax. Raum brings Fan Service to your nVidia GPU.
 No promises. But it just may work.
 
+![nvFanService-example](nvFanService-example.png)
+
 ## Currently:
 - Fan speeds are hard coded, but now we have a way to create and use custom fan curves... UwU
-- We're printing the temp and fan speed to the terminal every 10 seconds
-- It seems to work on Nobara 40 (Fedora 40 ala GloriusEggroll)
+- We're using Cursive to create an ncurses panel where we display the temp and fan speed, refreshed every 10 seconds, and I can invision a future in which one can use their own theme from a file.
+- Testing: It seems to work on Nobara 40 (Fedora 40 ala GloriusEggroll)
 - Uses sudo crate to automatically prompt for password
   - This eases development as I can just `cargo run -r` without trying to `sudo` my `cargo`
-  - The code is really short at the moment and easy to peruse as I am just some guy on the internet and trust is not safe. Trust no one. They're coming for you, Barbara.
-- press 'q' then 'Enter' to quit.
+  - The code is really short at the moment and easy to peruse as I am just some guy on the internet and it's really quite mad to trust just some guy on the internet. Trust no one. They're coming for you, Barbara.
+- press 'q' to quit. No more 'q' then 'Enter' gargage.
+  - Side note, it's possible we should be setting the fans back to default on exit and I have not yet looked into this.  I don't know if manually set speeds have a timeout or if I set it to 30% manually it just stays at 30% until told otherwise. So... use at your own risk and whatnot. I've been running the update loop for a couple of days with no instability, and when BG3 gets going, those fans go right to the top. If you prefer the quiet, my hard coded settings may not be for you. I put almost no thought into them.
 
 ## Todo:
-- "GUI": Seems like a good excuse to learn ncurses or the like
+- "GUI": Cursive TUI seems viable. Need to add fan curve customization.
 - Hard coded "fan curve" is bad and the current values are for testing purposes so I can hear it working
   - [x] Made a kind of fan curve struct to pass to service_fans()... UwU
   - [x] Said struct is a sorted list of (temp, speed) tuples
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 use {
+  arrayvec::ArrayString,
+  cursive_core::style::{
+    BaseColor::*, Color::*, PaletteColor::*,
+  },
+  cursive::{
+    event::Event,
+    theme::Theme,
+    views::{
+      OnEventView, Panel, TextContent, TextView,
+    },
+    Cursive, CursiveExt, 
+  },
   nvml_wrapper::{
-    device::Device,
-    enum_wrappers::device::TemperatureSensor,
-    Nvml
+    device::Device, enum_wrappers::device::TemperatureSensor, error::NvmlError, Nvml
   },
   regex::Regex,
   std::{
     error::Error,
     ffi::OsStr,
     fs::read_to_string,
-    io,
     path::Path,
-    sync::mpsc,
-    sync::mpsc::{
-      Receiver, TryRecvError,
-    },
-    thread,
     time::Instant,
   },
   sudo,
@@ -23,97 +27,164 @@ use {
 
 fn main() -> Result<(), Box<dyn Error>> {
   sudo::escalate_if_needed()?;
-  let drv_ver = get_driver_version();
-  let libname = if drv_ver.is_empty() {
-    "libnvidia-ml.so".to_owned()
-  } else {
-    format!("libnvidia-ml.so.{}", drv_ver)
+  let nvml = init_nvml_so()?;
+  let mut curve = FanCurveUwU::new(); // Still hard coded, but we are getting there.
+  curve.add(10,  0)?;
+  curve.add(20, 30)?;
+  curve.add(30, 60)?;
+  curve.add(36, 70)?;
+  curve.add(40, 80)?;
+  curve.add(52, 90)?;
+  curve.add(58,100)?;
+  let mut fan_service = FanService {
+    nvml, card_idx: None, card_name: ArrayString::new(),
+    curve, instant: Instant::now(), first_time: FirstTime(true),
+    text: "".to_owned(),
   };
-  println!("Attempting to use {} as our NVML Library.", libname);
-  let init_result = Nvml::builder().lib_path(OsStr::new(&libname)).init();
-  if let Ok(nvml) = init_result {
-    let mut fan_curve_uwu = FanCurveUwU::new(); // Still hard coded, but we are getting there.
-    fan_curve_uwu.add(10,  0)?;
-    fan_curve_uwu.add(20, 30)?;
-    fan_curve_uwu.add(30, 60)?;
-    fan_curve_uwu.add(36, 70)?;
-    fan_curve_uwu.add(40, 80)?;
-    fan_curve_uwu.add(52, 90)?;
-    fan_curve_uwu.add(58,100)?;
-    let fan_curve_uwu = fan_curve_uwu; // lock it down... for reasons.
-    let device_count = nvml.device_count();
-    if device_count.is_ok() {
-      let device_count = device_count.unwrap();
-      if device_count == 1 {
-        println!("Found one nVidia GPU.");
-        if let Ok(device) = nvml.device_by_index(0) {
-          let name = device.name().unwrap_or("<Unable to get device name>".to_owned());
-          println!("~> {}", &name);
-          return fan_service(device, fan_curve_uwu);
+  let name = { // Once we get the card name, we want to reuse it elsewhere.
+    // If card_idx is None, as it is before we get here,
+    // running fan_service.device()? picks the nVidia card
+    // we're going to use and fills in fan_service.card_name
+    // and .card_idx to the name and index of the chosen card.
+    let _ = fan_service.device()?;
+    fan_service.card_name.as_str().to_owned()
+  };
+  let mut siv = Cursive::new();
+  let content = TextContent::new("  Temp: ??, Fan Speed: ???  ");
+  let content_c = content.clone();
+  siv.set_user_data(fan_service);
+  siv.with_theme(|theme: &mut Theme| { // One day, this could be customized.
+    theme.palette[Background] = Dark(Black);
+    theme.palette[Shadow] = Rgb(30, 0, 0);
+    theme.palette[View] = Rgb(15, 25, 65);
+    theme.palette[Primary] = Rgb(0, 200, 0);
+    theme.palette[TitlePrimary] = Rgb(0, 100, 0);
+  });
+  siv.add_layer(Panel::new(
+    // I struggled for 2 nights after work trying to get a game like event loop running without
+    // using a whole bloody game engine. I just wanted to check the fans every 10 secs and display
+    // the temp and fan speed. I started with a global callback on Event::Refresh but it did not
+    // seem to be executing the code. Finding OnEventView(content).on_event(Event::Refresh, ...)
+    // was the first step. I had to put everything I might need into a single struct in order to
+    // do what must be done from within the closure used by on_event(). I started off trying to
+    // get the Device from Nvml and put it in the struct, but the Borrow Checker said no. I think
+    // it's because Device has &Nvml in its struct, which means it's pointing back to the original
+    // Nvml struct from whence it came.  So I had to reason with myself... I was trying to not have
+    // to call nvml.device(idx) every time I wanted access to the device. I wanted to just hang onto
+    // my Device that required me to "carefully" unwrap a Result to get. I was being ridiculous.
+    // I'm checking the temp every 10 seconds and getting the Device by index does not take some
+    // absurd amount of time. Going from nothing to having the temp known and the fan speed set
+    // according to that temp feels pretty instantaneous regardless of how many question marks and
+    // unwraps seem to be in the way. Those are just my promises to the compiler that everything
+    // is going to be fine. trust_me!(sus_code)
+    OnEventView::new(
+      TextView::new_with_content(content.clone())
+    ).on_event(Event::Refresh, move |s| {
+      s.with_user_data(|fs: &mut FanService| {
+        if fs.first_time.0 { // We don't want to wait 10 secs for our first service
+          fs.first_time.0 = false;
+          fs.service_service().unwrap();
+          return;
         }
-      } else if device_count > 1 {
-        println!("Found {} nVidia devices.\nPlease choose one:", device_count);
-        // List found nVidia devices
+        if fs.instant.elapsed().as_secs() >= 10 {
+          fs.service_service().unwrap();
+          fs.instant = Instant::now();
+        }
+      });
+      let txt: String = s.user_data::<FanService>().unwrap().text.clone();
+      if txt.len() > 0 { // This is probably not necessary, but neither was using Cursive
+        content_c.set_content(&txt);
       } else {
-        println!("NVML loaded but no nVidia GPUs found.");
+        content_c.set_content("  Temp: ??C, Fan Speed: ???%  ");
       }
-    }
-  } else if let Err(e) = init_result {
-    println!("Failed to load NVML.\n~> {}", e);
-  }
+    })
+  ).title(name));
+  siv.add_global_callback('q', |s| s.quit());
+  siv.set_fps(10);
+  siv.set_autorefresh(true);
+  siv.run();
   Ok(())
 }
 
-fn fan_service(device: Device, fan_curve_uwu: FanCurveUwU) -> Result<(), Box<dyn Error>> {
-  let name = device.name()?;
-  println!("Found {} fan(s) on {}.", device.num_fans()?, name);
-  service_fans(&device, &fan_curve_uwu)?;
-  let mut last_checked = Instant::now();
-  println!("Temp: {}, Fan Speed: {}",
-    device.temperature(TemperatureSensor::Gpu)?, device.fan_speed(0)?);
-  let stdin_channel = spawn_stdin_channel(); // Thanks, Stack Overflow!
-  'run_loop: loop {
-    match stdin_channel.try_recv() { // I hate this because I don't want to have to hit enter.
-      Ok(key) => {
-        println!("Shutting down...");
-        if key.starts_with("q") { break 'run_loop; }
-      }
-      Err(TryRecvError::Empty) => {}
-      Err(TryRecvError::Disconnected) => {
-        println!("Channel disconnected");
-        break 'run_loop;
-      }
-    }
-    if last_checked.elapsed().as_secs() >= 10 {
-      last_checked = Instant::now();
-      // It's been 10s, do you know what your temperature is?
-      service_fans(&device, &fan_curve_uwu)?;
-    }
-  }
-  Ok(())
+struct FanService {
+  nvml: Nvml,
+  card_idx: Option<u32>,
+  card_name: ArrayString<32>,
+  curve: FanCurveUwU,
+  instant: Instant,
+  first_time: FirstTime,
+  text: String,
 }
-
-fn service_fans(device: &Device, fan_curve_uwu: &FanCurveUwU) -> Result<(), Box<dyn Error>> {
-  // if the 1st GPU is not the one we want to control, can we TemperatureSensor::Gpu + 1 ??? 
-  let Ok(idx) = device.index() else { return Err("Failed to get index from device in service_fans()")? };
-  let Ok(fan_count) = device.num_fans() else { return Err("Failed to get num_fans from device in service_fans()")? };
-  let Ok(gpu_idx) = TemperatureSensor::try_from(idx) else { return Err("Failed to convert device index to TemperatureSensor enum in service_fans()")? };
-  let Ok(temp) = device.temperature(gpu_idx) else { return Err("Failed to get temperature reading from device in service_fans()")? };
-  for ts in &fan_curve_uwu.points {
-    if temp >= ts.temp() {
-      for idx in 0..fan_count {
-        if device.fan_speed(idx)? != ts.speed() {
-          device.set_fan_speed(idx, ts.speed())?;
+impl FanService {
+  // fn set_card_id(&mut self, idx: u32) { self.card_idx = Some(idx); }
+  
+  fn service_service(&mut self) -> Result<(), Box<dyn Error>> {
+    // if the 1st GPU is not the one we want to control, can we TemperatureSensor::Gpu + 1 ???
+    let Ok(fan_count) = self.device()?.num_fans() else { return Err("Failed to get num_fans from device in service_fans()")? };
+    let Ok(gpu_idx) = TemperatureSensor::try_from(self.card_idx.unwrap()) else { return Err("Failed to convert device index to TemperatureSensor enum in service_fans()")? };
+    let Ok(temp) = self.device()?.temperature(gpu_idx) else { return Err("Failed to get temperature reading from device in service_fans()")? };
+    let n: usize = self.curve.points.len();
+    for ts in 0..n {
+      if temp >= self.curve.points[ts].temp() {
+        for idx in 0..fan_count {
+          if self.device()?.fan_speed(idx)? != self.curve.points[ts].speed() {
+            let spd: u32 = self.curve.points[ts].speed();
+            self.device()?.set_fan_speed(idx, spd)?;
+          }
+        }
+        self.text = format!("  Temp: {}C, Fan Speed: {:>3}%  ", temp, self.curve.points[ts].speed());
+        return Ok(());
+      }
+    }
+    Err("Nothing happened, I swear!")?
+  }
+  
+  fn device(&mut self) -> Result<Device, NvmlError> {
+    if self.card_idx.is_some() { return self.nvml.device_by_index(self.card_idx.unwrap()) }
+    let device_count = self.nvml.device_count().unwrap_or(0);
+    if device_count == 0 { return Err(NvmlError::NotFound) }
+    if device_count == 1 {
+      println!("Found one nVidia GPU.");
+      let device = self.nvml.device_by_index(0);
+      if device.is_ok() {
+        let name = device.as_ref().unwrap().name().unwrap_or("<Unable to get device name>".to_owned());
+        self.card_name.push_str(&name);
+        self.card_idx = Some(0);
+        println!("~> {}", &name);
+      }
+      return device;
+    } else if device_count > 1 {
+      println!("Found {} nVidia devices.\nPlease choose one:", device_count);
+      let mut devices = Vec::new();
+      for i in 0..device_count {
+        let device = self.nvml.device_by_index(i);
+        if device.is_ok() {
+          let name = device.as_ref().unwrap().name().unwrap_or("<Unable to get device name>".to_owned());
+          self.card_name.push_str(&name);
+          println!("{} ~> {}", i + 1, &name);
+          devices.push((i, name));
         }
       }
-      println!("Temp: {}, Fan Speed: {}", temp, ts.speed());
-      break;
+      // fixme: defaulting to the first so I don't have to write a user prompt right now
+      println!("Picking a card not yet implemented.\nUsing first available card.");
+      if devices.len() > 0 {
+        self.card_idx = Some(devices[0].0);
+        self.card_name.push_str(&devices[0].1);
+        return self.nvml.device_by_index(0);
+      }
+      return Err(NvmlError::NotFound);
     }
+    return Err(NvmlError::NotFound);
   }
-  Ok(())
 }
 
-fn get_driver_version() -> String {
+fn init_nvml_so() -> Result<Nvml, NvmlError> {
+  let init_result = Nvml::init();
+  if init_result.is_ok() { return init_result }
+  // We're still here? libnvidia-ml.so is in another castle
+  // Attempt to locate libnvidia-ml.so.<current driver triple>
+  println!("Default libnvidia-ml.so not found.");
+  println!("Attempting to load libnvidia-ml.so.<current driver triple>");
   let file = Path::new("/proc/driver/nvidia/version");
   if let Ok(true) = Path::try_exists(&file) {
     let drv_ver_info = read_to_string(file).unwrap();
@@ -123,7 +194,10 @@ fn get_driver_version() -> String {
       if res.len() > 1 {
         let ver = res[1].to_owned();
         println!("Found nVidia driver version: {}", ver);
-        return ver;
+        let libname = format!("libnvidia-ml.so.{}", ver);
+        println!("Initializing with {}", &libname);
+        let init_result = Nvml::builder().lib_path(OsStr::new(&libname)).init();
+        if init_result.is_ok() { return init_result }
       }
     } else {
       println!("/proc/driver/nvidia/version exists but finding driver triple failed.");
@@ -133,18 +207,11 @@ fn get_driver_version() -> String {
   } else {
     println!("/proc/driver/nvidia/version not found.")
   }
-  return String::default()
-}
-
-fn spawn_stdin_channel() -> Receiver<String> {
-  // https://stackoverflow.com/questions/30012995/how-can-i-read-non-blocking-from-stdin
-  let (tx, rx) = mpsc::channel::<String>();
-  thread::spawn(move || loop {
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).unwrap();
-    tx.send(buffer).unwrap();
-  });
-  rx
+  
+  let libname = "libnvidia-ml.so.1".to_owned();
+  println!("Attempting to use {} as our NVML Library.", libname);
+  let init_result = Nvml::builder().lib_path(OsStr::new(&libname)).init();
+  return init_result
 }
 
 struct TempSpeed(u32,u32);
@@ -189,3 +256,6 @@ impl FanCurveUwU {
     Ok(())
   }
 }
+
+#[derive(Clone, Copy)]
+struct FirstTime(bool);
